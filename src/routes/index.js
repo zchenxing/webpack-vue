1128dffe46da3456149fd3cf0c4d5a2202808eb8
zyj7815 <yuanjie.zhu@ktbhealth.com>
2018-07-27 14:15:08 +0800

    配置路由

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 
 const Main = resolve => {require(['../views/main/main.vue'], resolve)}
 const Home = resolve => {require(['../views/home/home.vue'], resolve)}
+const My = resolve => {require(['../views/my/my.vue'], resolve)}
 
 Vue.use(Router)
 
@@ -10,7 +11,8 @@ export default new Router({
     routes: [
         { path: '/', component: Main,
             children: [
-                {path: '', component: Home }
+                {path: '', component: Home},
+                {path: '/my', component: My},
             ]
         }
     ]
--- src/views/layouts/footer.vue
+++ src/views/layouts/footer.vue
@@ -1,17 +1,54 @@
 <template>
     <div class="footer-view">
+    
+        <div class="footer-view__item" v-for="(item, index) in tabbar" :key="index" :class="{active: index === tabbarIndex}" :data-name="item.link" v-on:click="handleSelect(index)">
+            <div>
+                <img v-if="index === tabbarIndex" :src="item.select_img_url" alt="">
+                <img v-else :src="item.img_url" alt="">
+            </div>
+            <p>{{item.name}}</p>
+        </div>
+    
     </div>
 </template>
 
 
 
 <script>
-    
     export default {
         data() {
             return {
+                tabbarIndex: 0,
+                tabbar: [{
+                        name: "首页",
+                        link: "/",
+                        img_url: require("../../assets/images/home.png"),
+                        select_img_url: require("../../assets/images/home_select.png"),
+                    },
+                    {
+                        name: "我的",
+                        link: "/my",
+                        img_url: require("../../assets/images/my.png"),
+                        select_img_url: require("../../assets/images/my_select.png")
+                    }
+                ]
+            };
+        },
+        mounted() {
+            console.log(this.$route.path);
+
+            let _this = this;
+            this.tabbar.forEach((tab, index) => {
+                if(tab.link === _this.$route.path) { 
+                    _this.tabbarIndex = index;
+                }
+            })
+        },
+        methods: {
+            handleSelect(index) {
+                this.tabbarIndex = index;
+                this.$router.push(this.tabbar[index].link);
             }
         }
-    }
-
+    };
 </script>
